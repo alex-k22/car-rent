d1b6148a6dbfe2d8000e95bc021824ed7dfd839e
Alexey Kovalov <alexey.kovalov@gmail.com>
2023-09-16 00:52:56 +0300

    main.jsx path changed

--- src/data/header.js
+++
@@ -1,5 +0,0 @@
-export const headerNavData = [
-  { href: '', text: 'Home' },
-  { href: 'catalog', text: 'Catalog' },
-  { href: 'favorite', text: 'Favorite' },
-];
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/car-rent/",
+  base: "/car-rent",
   resolve: {
     alias: {
       components: "/src/components",
